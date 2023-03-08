f973ac45ae3120cf740025f39764650dd2e118a5
Piet Schijven <piet.schijven@innoq.com>
2023-03-08 11:43:11 +0100

    Initial commit

---
+++ src/main/java/pietschijven/openrewritespringdemo/OpenrewriteSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package pietschijven.openrewritespringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenrewriteSpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenrewriteSpringDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pietschijven/openrewritespringdemo/OpenrewriteSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pietschijven.openrewritespringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenrewriteSpringDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
